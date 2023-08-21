ecfa72db6c976e0a2aedfe46cccc1ae9123ac386
tim85145 <tim85145@gmail.com>
2023-08-21 21:43:19 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, abort
+
+from line_bot_api import *
+
+
+app = Flask(__name__)
+
+@app.route("/callback", methods = ['POST'])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret")
+        abort(400)
+
+    return 'OK'
+
+# 初始化事件處理器(也就是每次加入LineBot時做的事情，包含封鎖及解封)
+@handler.add(FollowEvent)
+def handle_follow(event):
+
+    # LineBot加入不同的環境(群組or好友)時的狀況
+    if event.source.type == Event.SOURCE_GROUP:
+        
+        # 加入群組時的回復內容
+        msg = '''歡迎加入群組'''    #這段要改，只是我目前懶得想歡迎詞
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=msg)
+        )
+
+    elif event.source.type == Event.SOURCE_USER:
+        
+        # 加入好友時的回復內容
+        msg = '''歡迎加入好友'''    #這段要改，只是我目前懶得想歡迎詞
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=msg)
+        )
+
+# 文字訊息處理器(每次收到文字訊息時的動作)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    pass
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ line_bot_api.py
@@ -0,0 +1,17 @@
+from linebot import LineBotApi, WebhookHandler  #連接Line Bot的兩個函數
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (MessageEvent, TextMessage, FollowEvent, Event, TextSendMessage)
+# from linebot.models import (MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, 
+#                             StickerSendMessage, ImageSendMessage, LocationSendMessage, FlexSendMessage,
+#                             TemplateSendMessage, ImageCarouselTemplate, ImageCarouselColumn, PostbackAction,
+#                             PostbackEvent, QuickReplyButton, QuickReply, ConfirmTemplate, ButtonsTemplate,
+#                             MessageAction)
+
+
+#取得這個Lint Bot的訪問全限，取得路徑：Basic Setting -> Channel Secret
+ChannelSecret = '810d353ee2085438f1bd57484ab8de89'
+#調用Line Bot的Messagr API，取得路徑：Messaging API -> Channel access token
+ChannelAccessToken = 'k6lNLdluZ7iQNGc4bgfhK5Aj7eoVUF7HsPqjYYDUv/yirvy59vs5sjlWJ7h4M/L3xRryRnKFzrGtMSLYSPpxPPGq1zIBpet5EkFF2iN1vYg3z/apw3TvShB+jWARRLJivsRx8a0I/Tlmq7CasOESqAdB04t89/1O/w1cDnyilFU='
+
+handler = WebhookHandler(ChannelSecret)
+line_bot_api = LineBotApi(ChannelAccessToken)
