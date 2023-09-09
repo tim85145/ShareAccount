6a62cee25a14277dadf4df809b256ff5ffc3315b
tim85145 <tim85145@gmail.com>
2023-09-09 12:09:50 +0800

    Add files via upload

--- line_bot_api.py
+++ line_bot_api.py
@@ -1,6 +1,6 @@
 from linebot import LineBotApi, WebhookHandler  #連接Line Bot的兩個函數
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import (MessageEvent, TextMessage, FollowEvent, JoinEvent, TextSendMessage)
+from linebot.models import (MessageEvent, TextMessage, FollowEvent, JoinEvent, TextSendMessage, PostbackEvent)
 # from linebot.models import (MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, 
 #                             StickerSendMessage, ImageSendMessage, LocationSendMessage, FlexSendMessage,
 #                             TemplateSendMessage, ImageCarouselTemplate, ImageCarouselColumn, PostbackAction,
